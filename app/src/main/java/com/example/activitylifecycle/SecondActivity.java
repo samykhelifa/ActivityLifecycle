3bab228baf661a5dae9a8bc73cd6f9c88aa1af35
samy <ryio69007@gmail.com>
2019-07-15 09:38:15 +0200

    after        modified:   app/src/main/AndroidManifest.xml         modified:   app/src/main/java/com/example/activitylifecycle/MainActivity.java         new file:   app/src/main/java/com/example/activitylifecycle/SecondActivity.java         new file:   app/src/main/res/layout/activity_second.xml         modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/activitylifecycle/SecondActivity.java
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.java
@@ -1,62 +1,51 @@
 package com.example.activitylifecycle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate()");
-    }
+        setContentView(R.layout.activity_second);
 
+    }
     @Override
     protected void onRestart() {
         super.onRestart();
-
         Log.i(TAG, "onRestart()");
     }
-
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart()");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "onResume()");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause()");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop()");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy()");
     }
 
-    public void openSecondActivity(View view) {
-        Intent intent= new Intent(this,SecondActivity.class);
-        startActivity(intent);
-    }
 }
+
